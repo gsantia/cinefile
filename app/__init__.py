bf3ebc18deea6ca9822fd6d83ffac0e3e1aea4f5
gsantia <gs495@drexel.edu>
2019-09-21 00:02:48 -0400

    added user registration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 ########################################################################
 # This script simply creates the application object as an instance of 
@@ -12,5 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime
 from app import db
+
+# User login loader
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 ###############################################################
 # Create the database models here
 ###############################################################
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
@@ -14,6 +23,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key = True)
