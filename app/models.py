3aa70076813f6646a81b72a5d9f87110f12e876f
gsantia <gs495@drexel.edu>
2019-09-22 23:52:20 -0400

    added user profiles

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from hashlib import md5
 from app import db
 
 # User login loader
@@ -15,11 +16,16 @@ def load_user(id):
 ###############################################################
 
 class User(UserMixin, db.Model):
+    """ 
+    Handles the users of the app.
+    """
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
     reviews = db.relationship('Review', backref = 'author', lazy = 'dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -30,7 +36,15 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                    digest, size)
+
 class Review(db.Model):
+    """
+    Represents a film review created by a user.
+    """
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.Text)
