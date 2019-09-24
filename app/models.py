c691fa4de6433a173f33f9991c1db8f927a4276f
gsantia <gs495@drexel.edu>
2019-09-24 14:51:33 -0400

    oop

--- app/models.py
+++ app/models.py
@@ -40,7 +40,14 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
                     digest, size)
-# Handle followers
+
+    followed = db.relationship(
+        'User', secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'), lazy = 'dynamic')
+
+# Followers association table
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
