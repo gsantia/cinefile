eeffbc0c91d170a16fe2cedc8b730911237d82df
gsantia <gs495@drexel.edu>
2019-09-26 22:39:51 -0400

    tried to add email

--- config.py
+++ config.py
@@ -3,8 +3,19 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # SQL Stuff
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATEBASE_URL') or \
         'sqlite:///' + os.path.join(base_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Email stuff
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['gs495@drexel.edu']
+
 
     POSTS_PER_PAGE = 3
