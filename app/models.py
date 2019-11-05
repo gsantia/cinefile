1e3026d47e1bbf1f7480955aa3b9f9020ce084be
gsantia <gs495@drexel.edu>
2019-11-05 01:57:39 -0500

    finished flask mega-tutorial!

--- app/models.py
+++ app/models.py
@@ -3,7 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app, url_for
 from flask_login import UserMixin
 import jwt
-from datetime import datetime
+import base64
+from datetime import datetime, timedelta
+import os
 from time import time
 from hashlib import md5
 import json
@@ -72,6 +74,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     last_message_read_time = db.Column(db.DateTime)
     notifications = db.relationship('Notification', backref = 'user', lazy = 'dynamic')
     tasks = db.relationship('Task', backref = 'user', lazy = 'dynamic')
+    token = db.Column(db.String(32), index = True, unique = True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -150,7 +154,6 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
 
     def get_task_in_progress(self, name):
         return Task.query.filter_by(name = name, user = self, complete = False).first()
-
     
     # API helper functions
     def to_dict(self, include_email = False):
@@ -180,6 +183,28 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    
+    def get_token(self, expires_in = 3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds = 60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds = expires_in)
+        db.session.add(self)
+        return self.token
+
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds = 1)
+
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token = token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 class Review(db.Model):
